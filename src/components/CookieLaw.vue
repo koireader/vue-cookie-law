a65bb1b6b1fe39c1aed997fbef05f88d1075e941
b4bhagath <nikhil.bhagath@asepsislife.com>
2018-05-30 20:06:50 +0530

    Added checkbox and vuetify

--- src/components/CookieLaw.vue
+++ src/components/CookieLaw.vue
@@ -1,13 +1,22 @@
 <template>
   <transition appear :name="transitionName">
     <div class="Cookie" :class="[containerPosition, cookieTheme]" v-if="isOpen">
-      <div class="Cookie__content">
-        <slot name="message">{{ message }}</slot>
-      </div>
-      <div class="Cookie__buttons">
-        <a :target="target" :href="buttonLink" v-if="externalButtonLink" :class="buttonClass">{{ buttonLinkText }}</a>
-        <router-link :to="buttonLink" v-if="internalButtonLink" :class="buttonClass">{{ buttonLinkText }}</router-link>
-        <div :class="buttonClass" @click="accept">{{ buttonText }}</div>
+      <v-layout class="Cookie_header">
+        <v-flex class="text-xs-left Cookie__content">
+          <slot name="message" class="headline">{{ message }}</slot>
+        </v-flex>
+      </v-layout>
+      <div class="Cookie_checkbox">
+        <v-layout row class="Cookie__buttons">
+          <v-flex xs1><v-checkbox disabled label="Necessary" v-model="selected" value="Necessary"></v-checkbox></v-flex>
+          <v-flex xs1 class="mx-auto"><v-checkbox label="Statistics" v-model="selected" value="Statistics"></v-checkbox></v-flex>
+          <v-flex xs1><v-checkbox label="Others" v-model="selected" value="Others"></v-checkbox></v-flex>
+          <v-flex class="text-xs-right">
+            <!-- <a :target="target" :href="buttonLink" v-if="externalButtonLink" :class="buttonClass">{{ buttonLinkText }}</a>
+            <router-link :to="buttonLink" v-if="internalButtonLink" :class="buttonClass">{{ buttonLinkText }}</router-link> -->
+            <v-btn depressed :class="buttonClass" @click="accept">{{ buttonText }}</v-btn>        
+          </v-flex>
+        </v-layout>
       </div>
     </div>
   </transition>
@@ -35,7 +44,7 @@
       },
       message: {
         type: String,
-        default: 'This website uses cookies to ensure you get the best experience on our website.'
+        default: 'We use technologies, such as cookies, to customise content and advertising, to provide social media features and to analyse traffic to the site. We also share information about your use of our site with our trusted social media, advertising and analytics partners.'
       },
       theme: {
         type: String,
@@ -69,7 +78,8 @@
     data () {
       return {
         supportsLocalStorage: true,
-        isOpen: false
+        isOpen: false,
+        selected: ['Necessary']
       }
     },
     computed: {
@@ -123,126 +133,151 @@
       accept () {
         this.setVisited()
         this.isOpen = false
-        this.$emit('accept')
+        this.$emit('accept', this.selected)
       }
     }
   }
 </script>
 
 <style lang="scss">
-  @import "~@nextindex/next-scss/next-scss.scss";
+@import "~@nextindex/next-scss/next-scss.scss";
+@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
 
-  .Cookie {
-    position: fixed;
-    overflow: hidden;
-    box-sizing: border-box;
-    z-index: 9999;
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: baseline;
-    flex-direction: column;
+.Cookie {
+  position: fixed;
+  overflow: hidden;
+  box-sizing: border-box;
+  z-index: 9999;
+  width: 100%;
+  flex-direction: column;
 
-    > * {
-      margin: rem(15) 0;
-      align-self: center;
-    }
-
-    @include media($sm-up) {
-      flex-flow: row;
-
-      > * {
-        margin: 0;
-      }
-    }
-  }
-
-  .Cookie--top {
-    top: 0;
-    left: 0;
-    right: 0;
-  }
-
-  .Cookie--bottom {
-    bottom: 0;
-    left: 0;
-    right: 0;
-  }
-  .Cookie__buttons {
-    display: flex;
-    flex-direction: column;
-
-    > * {
-      margin: rem(5) 0;
-    }
-
-    @include media($sm-up) {
-      flex-direction: row;
-      > * {
-        margin: 0 rem(15);
-      }
-    }
-  }
-  .Cookie__button {
-    cursor: pointer;
+  > * {
+    margin: rem(15) 0;
     align-self: center;
   }
 
-  @mixin generateTheme($theme, $backgroundColor, $fontColor, $buttonBackgroundColor, $buttonFontColor: #fff, $buttonRadius: 0) {
-    .Cookie--#{$theme} {
-      background: $backgroundColor;
-      color: $fontColor;
-      padding: 1.250em;
+  @include media($sm-up) {
+    flex-flow: row;
 
-        .Cookie__button {
-          background: $buttonBackgroundColor;
-          padding: 0.625em 3.125em;
-          color: $buttonFontColor;
-          border-radius: $buttonRadius;
+    > * {
+      margin: 0;
+    }
+  }
+}
 
-          &:hover {
-            background: darken($buttonBackgroundColor, 10%);
-          }
+.Cookie--top {
+  top: 0;
+  left: 0;
+  right: 0;
+}
+
+.Cookie--bottom {
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+.Cookie__buttons {
+  display: flex;
+  flex-direction: column;
+
+  > * {
+    margin: rem(5) 0;
+  }
+
+  @include media($sm-up) {
+    flex-direction: row;
+    > * {
+      margin: 0 rem(15);
+    }
+  }
+}
+.Cookie__button {
+  cursor: pointer;
+  align-self: center;
+}
+.Cookie_checkbox {
+  display: flex;
+}
+.Cookie_header {
+  display: flex;
+  padding: 20px 0;
+  // justify-content: space-between;
+  align-items: baseline;
+}
+
+@mixin generateTheme(
+  $theme,
+  $backgroundColor,
+  $fontColor,
+  $buttonBackgroundColor,
+  $buttonFontColor: #fff,
+  $buttonRadius: 0
+) {
+  .Cookie--#{$theme} {
+    background: $backgroundColor;
+    color: $fontColor;
+    padding: 1.25em;
+    .Cookie__button {
+      background: $buttonBackgroundColor;
+      padding: 0 3.125em;
+      color: $buttonFontColor;
+      border-radius: $buttonRadius;
+      &:hover {
+        background: darken($buttonBackgroundColor, 10%);
       }
     }
   }
-
-  @include generateTheme('base', #F1F1F1, #232323, #97D058);
-  @include generateTheme('base--rounded', #F1F1F1, #232323, #97D058, #fff, 20px);
-  @include generateTheme('blood-orange', #424851, #fff, #E76A68);
-  @include generateTheme('blood-orange--rounded', #424851, #fff, #E76A68, #fff, 20px);
-  @include generateTheme('dark-lime', #424851, #fff, #97D058);
-  @include generateTheme('dark-lime--rounded', #424851, #fff, #97D058, #fff, 20px);
-  @include generateTheme('royal', #FBC227, #232323, #726CEA, #fff);
-  @include generateTheme('royal--rounded', #FBC227, #232323, #726CEA, #fff, 20px);
-
-  .slideFromTop-enter, .slideFromTop-leave-to {
-    transform: translate(0px, -12.500em);
-  }
-
-  .slideFromTop-enter-to, .slideFromTop-leave {
-    transform: translate(0px, 0px);
-  }
-
-  .slideFromBottom-enter, .slideFromBottom-leave-to {
-    transform: translate(0px, 12.500em);
-  }
-
-  .slideFromBottom-enter-to, .slideFromBottom-leave {
-    transform: translate(0px, 0px);
-  }
-
-  .slideFromBottom-enter-active,
-  .slideFromBottom-leave-active,
-  .slideFromTop-enter-active,
-  .slideFromTop-leave-active, {
-    transition: transform .4s ease-in;
-  }
-
-  .fade-enter-active, .fade-leave-active {
-    transition: opacity .5s
-  }
-  .fade-enter, .fade-leave-to {
-    opacity: 0
-  }
+}
+@include generateTheme("base", #f1f1f1, #232323, #97d058);
+@include generateTheme("base--rounded", #f1f1f1, #232323, #97d058, #fff, 20px);
+@include generateTheme("blood-orange", #424851, #fff, #ad2624);
+@include generateTheme(
+  "blood-orange--rounded",
+  #424851,
+  #fff,
+  #e76a68,
+  #fff,
+  20px
+);
+@include generateTheme("dark-lime", #424851, #fff, #97d058);
+@include generateTheme(
+  "dark-lime--rounded",
+  #424851,
+  #fff,
+  #97d058,
+  #fff,
+  20px
+);
+@include generateTheme("royal", #fbc227, #232323, #726cea, #fff);
+@include generateTheme("royal--rounded", #fbc227, #232323, #726cea, #fff, 20px);
+.slideFromTop-enter,
+.slideFromTop-leave-to {
+  transform: translate(0px, -12.5em);
+}
+.slideFromTop-enter-to,
+.slideFromTop-leave {
+  transform: translate(0px, 0px);
+}
+.slideFromBottom-enter,
+.slideFromBottom-leave-to {
+  transform: translate(0px, 12.5em);
+}
+.slideFromBottom-enter-to,
+.slideFromBottom-leave {
+  transform: translate(0px, 0px);
+}
+.slideFromBottom-enter-active,
+.slideFromBottom-leave-active,
+.slideFromTop-enter-active,
+.slideFromTop-leave-active {
+  transition: transform 0.4s ease-in;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App'
+import Vuetify from 'vuetify'
 
+// Assuming you are using webpack, require the CSS file
+require('../node_modules/vuetify/dist/vuetify.min.css')
 Vue.config.productionTip = false
+
+Vue.use(Vuetify, {
+  theme: {
+    secondary: '#11151d'
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
